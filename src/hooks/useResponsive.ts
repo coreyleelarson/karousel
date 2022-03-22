ea6ac2acc36f15a14b9a519206a7f34884477a26
Corey Larson <coreyleelarson@gmail.com>
2022-03-22 17:57:55 -0500

    Adds responsive listeners.

--- src/hooks/useResponsive.ts
+++ src/hooks/useResponsive.ts
@@ -1,15 +1,32 @@
-import { useEffect, useState } from "react";
-import { KarouselOptions, ResponsiveOptions } from "../types";
+import { useCallback, useLayoutEffect, useMemo, useState } from "react";
+import { ResponsiveOptions } from "../types";
 
 export const useResponsive = (options: ResponsiveOptions) => {
   const { responsive } = options;
-  const [responsiveOptions, setResponsiveOptions] = useState<KarouselOptions>(options);
 
-  useEffect(() => {
+  // Sorts breakpoints ascending and adds a media query listener.
+  const parsedResponsive = useMemo(() => {
     const sorted = responsive.slice().sort((a, b) => a.breakpoint - b.breakpoint);
-    const matchedOptions = sorted.find(instance => window.matchMedia(`(min-width: ${instance.breakpoint}px)`).matches);
-    setResponsiveOptions({ ...options, ...matchedOptions?.options });
+    return sorted.map(instance => ({ ...instance, mql: window.matchMedia(`(min-width: ${instance.breakpoint}px)`) }));
   }, [responsive]);
+
+  // Gets the matched options based on media query listeners..
+  const getResponsiveOptions = useCallback(() => {
+    const matchedOptions = parsedResponsive.find(instance => instance.mql.matches)?.options;
+    return { ...options, ...matchedOptions };
+  }, [options, parsedResponsive]);
+
+  const [responsiveOptions, setResponsiveOptions] = useState(getResponsiveOptions);
+
+  useLayoutEffect(() => {
+    if (typeof window !== 'undefined') {
+      const handler = () => setResponsiveOptions(getResponsiveOptions);
+      parsedResponsive.forEach(instance => instance.mql.addEventListener('change', handler));
+      return () => {
+        parsedResponsive.forEach(instance => instance.mql.removeEventListener('change', handler));
+      }
+    }
+  }, [parsedResponsive]);
 
   return { responsiveOptions };
 }
