f9c3f19de997e716cea64e819a2cc1e0b56f807f
Corey Larson <coreyleelarson@gmail.com>
2022-03-22 12:43:41 -0500

    Reorders keys.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,12 @@
 module.exports = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
-  framework: "@storybook/react",
   core: {
     builder: "storybook-builder-vite",
   },
+  framework: "@storybook/react",
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
 };
