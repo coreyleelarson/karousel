69ecac903552e907de97d79b009fbdf7f0c2face
Corey Larson <coreyleelarson@gmail.com>
2022-03-26 13:50:54 -0500

    Updates docs styling.

--- apps/docs/app/components/Header/index.tsx
+++ apps/docs/app/components/Header/index.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/docs">Docs</Link>
+            <Link to="/docs/overview">Docs</Link>
           </li>
           <li>
             <Link to="/demo">Demo</Link>
